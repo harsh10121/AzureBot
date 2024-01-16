40feb7bf92f99da87bc3d5476d584c183a96bf5d
harsh10121 <93761689+harsh10121@users.noreply.github.com>
2024-01-17 02:23:53 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -15,13 +15,13 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 const corsOptions = {
-    origin: "https://good-space-task.vercel.app",
+    origin: "https://azure-bot-h.vercel.app",
     methods: ["POST", "GET"],
     credentials: true
 };
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://good-space-task.vercel.app");
+    res.header("Access-Control-Allow-Origin", "https://azure-bot-h.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET, POST");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", 'true');
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-    origin:["https://good-space-task.vercel.app"],
+    origin:["https://azure-bot-h.vercel.app"],
     methods:["POST","GET"],
     credentials: true
 }));
